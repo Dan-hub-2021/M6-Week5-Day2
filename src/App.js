f999d2041a2f3ed67050fcd93194464e6bf7e543
Dan Sy <dan.toansy@westcliff.edu>
2021-06-06 21:30:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import InventoryList from './InventoryList';
+import InventoryEdit from './InventoryEdit';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/inventories' exact={true} component={InventoryList}/>
+          <Route path='/inventories/:id' component={InventoryEdit}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
